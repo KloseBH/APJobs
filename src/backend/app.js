9682fae8ef44339a2046a828073974fbd2908baa
Daniel Henrique Alves Bicalho Dias <daniel.dias@4mti.com.br>
2025-08-05 10:14:47 -0300

    #0007 - Feature: Coleta de vagas da Remotive e Mensagens de alerta para o usuário.

--- src/backend/app.js
+++ src/backend/app.js
@@ -1,5 +1,6 @@
 const jobRoutes = require('./routes/index.js');
 const express = require('express');
+const cors = require('cors');
 
 const app = express();
 
--- src/backend/controllers/jobsController.js
+++ src/backend/controllers/jobsController.js
@@ -9,12 +9,14 @@ async function searchJobs(req, res) {
             });
         }
 
-        const jobsGupy = await crawlerGupy(req.query);
+        const jobsRemotive = await apiRemotive(req.query);
+        const jobsGupy = await crawlerGP(req.query);
         jobs = {
+            Remotive: jobsRemotive?jobsRemotive:[],
             Gupy: jobsGupy?jobsGupy:[]
         }
         res.status(200).json({
-            jobs
+            APJobs: jobs
         });
     } catch (error) {
         console.error('Erro ao buscar vagas:', error);
@@ -40,8 +42,12 @@ function parseTempo(tempoParam) {
   return null;
 }
 
-async function crawlerGupy(params){
+async function crawlerGP(params){
     console.log('Crawler Gupy...');
+
+    let json_response = {};
+    json_response['aviso'] = "As vagas coletadas a partir da plataforma Gupy são obtidas por meio de consultas públicas permitidas pelo arquivo robots.txt. A coleta é realizada com responsabilidade, respeitando limites técnicos e sem sobrecarregar os servidores. Utilizamos apenas endpoints acessíveis pelo próprio site da Gupy, com frequência controlada. Todas as vagas mantêm seu link original e podem ser acessadas diretamente pela plataforma oficial."
+    json_response['url_portal'] = 'https://gupy.io/';
 
     const location = params.location ? `&country=${params.location}` : ''; 
     const title = params.title.replace(/\s/g, '+');
@@ -54,7 +60,6 @@ async function crawlerGupy(params){
 
     let url;
     let jobs = {};
-
     try {
         let id = 0;
         console.log('Coletando ' + (page*100) + ' vagas');
@@ -107,21 +112,115 @@ async function crawlerGupy(params){
         }        
     } catch (error) {
         console.error('Erro no crawler da Gupy:', error);
-        return {
-            message: 'Erro ao buscar vaga na Gupy',
-            error: error
-        }
+        json_response['message'] = 'Erro ao buscar vaga na Gupy';
+        json_response['error'] = error;
+        return json_response;
     }
 
     if(!jobs || Object.keys(jobs).length === 0) {
-        return {
-            message: 'Nenhuma vaga encontrada na Gupy para o titulo: ' + params.title
-        }
+        json_response['message'] = 'Nenhuma vaga encontrada na Gupy para o titulo: ' + params.title;
+        return json_response;
     }
 
-    return jobs;
+    json_response['jobs'] = jobs;
+    return json_response;
 }
 
+async function apiRemotive(params) {
+    console.log('Acessado API da Remotive...');
+
+    let json_response = {};
+    json_response['aviso'] = "As vagas obtidas da plataforma Remotive são acessadas por meio de sua API pública, conforme permitido em sua documentação oficial. A coleta é realizada com moderação, respeitando os limites de requisições recomendados (até 4 vezes ao dia) e com atribuição adequada. Todas as vagas incluem o link original e mencionam a Remotive como fonte, garantindo transparência e conformidade com os termos de uso da plataforma.";
+    json_response['url_portal'] = 'https://remotive.com/';
+
+    const title = params.title.replace(/\s/g, '+');
+    const time = params.time ? parseTempo(params.time) : parseTempo('31d');
+    const location = params.location??null;
+    const page = params.page ? parseInt(params.page, 10) : 1;
+
+    const limit = 100;
+    let offset;
+    if(typeof page !== 'number') {
+        offset = 0;
+    }else{
+        offset = (page-1)*100;
+    }
+
+    let url;
+    let jobs = {};
+    try {
+        console.log('Coletando vagas da Remotive...');
+        setTimeout(() => {}, 500);
+        url = 'https://remotive.com/api/remote-jobs?search=' + title;
+        console.log(url);
+        
+        let response = await axios.get(url)
+                        .then(response => response)
+                        .catch(error => error.response);
+
+        if(response.status !== 200) {
+            console.log('Coletadas todas as vagas disponiveis na Remotive!');
+            json_response['message'] = 'Nenhuma vaga encontrada na Remotive para o titulo: ' + params.title;
+            return json_response;
+        }
+
+        const json = await response.data.jobs;
+
+        if(!json || json.length === 0) {
+            console.log('Coletadas todas as vagas disponiveis na Remotive!');
+            json_response['message'] = 'Nenhuma vaga encontrada na Remotive para o titulo: ' + params.title;
+            return json_response;
+        }
+
+        let id = 0;
+        let count = 0;
+        json.forEach(job => {
+            if (!job.publication_date) return;
+
+            if (offset > 0){
+                offset--;
+                return;
+            }; 
+            if (count >= limit) return;
+            count++;
+
+            const dataPublicada = new Date(job.publication_date);
+            const dataReferencia = (new Date() - dataPublicada) / 1000;
+
+            if (dataReferencia >= time) return;
+            if (location && !job.candidate_required_location.includes(location)) return;
+
+            jobs[id] = {
+                portal: 'Remotive',
+                title: job.title || 'Sem título',
+                type: 'Remoto',
+                description: job.description || '',
+                company: {
+                    name: job.company_name || 'Desconhecida'
+                },
+                location: job.candidate_required_location || '',
+                date: job.publication_date.split('T')[0],
+                link: job.url || ''
+            };
+
+            id++;
+        });
+        console.log('Coletadas ' + Object.keys(jobs).length + ' vagas');       
+    } catch (error) {
+        console.error('Erro ao coletar da API da Remotive:', error);
+        json_response['message'] = 'Erro ao buscar vaga na Remotive';
+        json_response['error'] = error;
+        return json_response;
+    }
+
+    if(!jobs || Object.keys(jobs).length === 0) {
+        json_response['message'] = 'Nenhuma vaga encontrada na Remotive para o titulo: ' + params.title;
+        return json_response;
+    }
+
+    json_response['jobs'] = jobs;
+    return json_response;
+}
 
 module.exports = {
     searchJobs
